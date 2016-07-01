ec83e05ec9fd9a07dd7612ceff77d19a67cd1bbd
isaranto <isarand@gmail.com>
2016-07-01 16:49:12 +0300

    [DEV] app is ready

--- search_page.py
+++ search_page.py
@@ -1,4 +1,3 @@
-# all the imports
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from elasticsearch import Elasticsearch
@@ -6,42 +5,62 @@ from contextlib import closing
 import requests, json
 
 
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
-
 @app.route('/')
-def show_entries(query=""):
-    #term = "MEASUREMENT OF DIELECTRIC CONSTANT OF LIQUIDS BY THE USE OF MICROWAVE TECHNIQUES"
-    total, results = custom_search(query, 0)
-    return render_template('search.html', results=results, total=total, query_text=query)
+def start_page(query="", page=1):
+    total, results = custom_search(query, page)
+    return render_template('search.html', results=results, total=total, query_text=query, page=page)
+
+
+@app.route('/<query>/<page>', methods=['GET', 'POST'])
+def show_entries(query="", page=1):
+    if request.method == 'POST':
+        query = request.form["search-text"]
+    total, results = custom_search(query, page)
+    return render_template('search.html', results=results, total=total, query_text=query, page=page)
 
 
 @app.route('/search', methods=['POST'])
 def search():
-     if request.method == 'POST':
-         text = request.form["search-text"]
-         total, results = custom_search(text, 0)
-         return render_template('search.html', results=results, total=total, query_text=text)
+    if request.method == 'POST':
+        text = request.form["search-text"]
+        total, results = custom_search(text, 1)
+        return render_template('search.html', results=results, total=total, query_text=text, page=1)
+
 
 @app.route('/relevant', methods=['POST'])
 def relevant():
-     if request.method == 'POST':
-         id = request.form["relevant"]
-         query_text = request.form["query_name"]
-         add_relevant_query(id, query_text)
-         return show_entries(query_text)
+    if request.method == 'POST':
+        id = request.form["relevant"]
+        query_text = request.form["query_name"]
+        page = request.form["page"]
+        add_relevant_query(id, query_text)
+        print id, query_text
+        return redirect(url_for('show_entries', query=query_text, page=page))
+
 
 @app.route('/note', methods=['POST'])
 def note():
-     if request.method == 'POST':
-         note = request.form["note"]
-         id = request.form["submit"]
-         query_text = request.form["query_name"]
-         add_note(id, note)
-         return show_entries(query_text)
+    if request.method == 'POST':
+        note = request.form["note"]
+        id = request.form["submit"]
+        page = request.form["page"]
+        query_text = request.form["query_name"]
+        add_note(id, note)
+        print id, note
+        return redirect(url_for('show_entries', query=query_text, page=page))
+
+
+@app.route('/page', methods=['POST'])
+def get_page():
+    if request.method == 'POST':
+        page = int(request.form["page"])
+        query_text = request.form["query_name"]
+        print query_text
+        return redirect('/'+query_text+'/'+int(page))
+
 
 def add_note(id, note):
     """
@@ -54,12 +73,13 @@ def add_note(id, note):
 
     """
     es = Elasticsearch(hosts='http://okeanos.gr:9200/')
-    es.update(index='documents',doc_type='doc',id=id,
-                body={"script" : "ctx._source.note += note",
-                        "params" : {
-                        "note" : [note]
+    es.update(index='documents', doc_type='doc', id=id,
+              body={"script": "ctx._source.note += note",
+                    "params": {
+                      "note": [note]
                         }
-                        })
+                    })
+
 
 def add_relevant_query(id, query):
     """
@@ -72,22 +92,28 @@ def add_relevant_query(id, query):
 
     """
     es = Elasticsearch(hosts='http://okeanos.gr:9200/')
-    es.update(index='documents',doc_type='doc',id=id,
-                body={"script" : "ctx._source.relq += relq",
-                        "params" : {
-                        "relq" : [query]
+    es.update(index='documents', doc_type='doc', id=id,
+              body={"script": "ctx._source.relq += relq",
+                    "params": {
+                      "relq": [query]
                         }
-                        })
+                    })
 
-def custom_search(query_text, start):
+
+def custom_search(query_text, page):
     """
-    Searches for the query provided
+    Connect to elasticsearch and search for query
     Args:
-        search_text: query
+        query_text: the query...
+        page: the page of the results to show
 
-    Returns: total number of hits, results
+    Returns:
 
     """
+    if page == 1:
+        start = 0
+    else:
+        start = (int(page)-1)*20
     es = Elasticsearch(hosts='http://okeanos.gr:9200/')
     res = es.search(index="documents", doc_type="doc", body={
             "query": {
@@ -96,8 +122,8 @@ def custom_search(query_text, start):
                     "fields": ["text^2", "relq^3", "note"]
                     }
                 }
-        }, from_ = start, size=30)
-    if res['hits']['hits'] !=0 :
+        }, from_=start, size=20)
+    if res['hits']['hits'] != 0:
         return res['hits']['total'], res['hits']['hits']
     else:
         return {}, 0
